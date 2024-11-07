fb4f95c3bd8ffcc43814aca196963a9e444c6904
John Michael Lacurom <lacuromjm48@gmail.com>
2024-11-07 22:30:29 +0800

    Update

--- php/connection.php
+++ php/connection.php
@@ -2,8 +2,8 @@
   try{
     $HOST_NAME = "localhost";
     $HOST_USER = "root";
-    $HOST_PASS = "";
-    $HOST_DBNAME = "skillset";
+    $HOST_PASS = "Skillset1234567890";
+    $HOST_DBNAME = "u434306754_skillset";
     $conn = mysqli_connect($HOST_NAME, $HOST_USER, $HOST_PASS, $HOST_DBNAME);
   }
   catch(Exception $e){
