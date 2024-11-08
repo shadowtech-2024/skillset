15cbb459b3ef62c2615c7e570c76e3abafcf978a
John Michael Lacurom <lacuromjm48@gmail.com>
2024-11-08 13:05:45 +0800

    added

---
+++ save_image.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['imgBase64'])) {
+    // Decode the base64 image
+    $imgData = $_POST['imgBase64'];
+    $imgData = str_replace('data:image/png;base64,', '', $imgData);
+    $imgData = str_replace(' ', '+', $imgData);
+    $decodedData = base64_decode($imgData);
+
+    // Generate a unique filename
+    $fileName = 'uploads/face_' . uniqid() . '.png';
+
+    // Save the image to the server
+    if (file_put_contents($fileName, $decodedData)) {
+        echo json_encode(["status" => "success", "fileName" => $fileName]);
+    } else {
+        echo json_encode(["status" => "error"]);
+    }
+} else {
+    echo json_encode(["status" => "no_data"]);
+}
+?>
